00e91d6391c61e535fc6704e7f0416ac8e2c712c
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2021-09-13 20:00:24 +0600

    create simple form validation

---
+++ js/form.js
@@ -0,0 +1,13 @@
+/// log in signup none block
+const addRemove = (idAdd,idRemove,className) =>{
+    document.getElementById(idAdd).classList.add(className);
+    document.getElementById(idRemove).classList.remove(className);
+}
+const log = () =>{
+    addRemove('login','signup','active')
+    addRemove('btn-active-log','btn-active-sign','btn-active');
+}
+const sign = () =>{
+    addRemove('signup','login','active')
+    addRemove('btn-active-sign','btn-active-log','btn-active');
+}
